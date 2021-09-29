7f75f754d6b11a554b022d488bdde069f1ed376b
witney2326 <wkabango@gmail.com>
2021-09-29 18:09:20 +0200

    additions

--- basic_livelihood.php
+++ basic_livelihood.php
@@ -5,6 +5,13 @@
     <title>Basic Livelihood</title>
     <?php include 'layouts/head.php'; ?>
     <?php include 'layouts/head-style.php'; ?>
+
+    <!-- DataTables -->
+    <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <!-- Responsive datatable examples -->
+    <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    
 </head>
 
 <?php include 'layouts/body.php'; ?>
@@ -122,17 +129,198 @@
                                             voluptate nisi qui.
                                         </p>
                                     </div>
-                                    <div class="tab-pane" id="profile-1" role="tabpanel">
+                                    <div class="tab-pane" id="Beneficiaries" role="tabpanel">
                                         <p class="mb-0">
-                                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla
-                                            single-origin coffee squid. Exercitation +1 labore velit, blog
-                                            sartorial PBR leggings next level wes anderson artisan four loko
-                                            farm-to-table craft beer twee. Qui photo booth letterpress,
-                                            commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
-                                            vinyl cillum PBR. Homo nostrud organic, assumenda labore
-                                            aesthetic magna 8-bit.
-                                        </p>
+                                        <form class="needs-validation" novalidate>
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <div class="mb-2">
+                                                        <label for="region" class="form-label">Region</label>
+                                                        <select class="form-select" id="region" required>
+                                                            <option selected disabled value="">Choose...</option>
+                                                            <option>...</option>
+                                                        </select>
+                                                        <div class="invalid-feedback">
+                                                            Please select a valid Malawi region.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="mb-2">
+                                                        <label for="district" class="form-label">District</label>
+                                                        <select class="form-select" id="district" required>
+                                                            <option selected disabled value="">Choose...</option>
+                                                            <option>...</option>
+                                                        </select>
+                                                        <div class="invalid-feedback">
+                                                            Please select a valid Malawi district.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="mb-2">
+                                                        <label for="ta" class="form-label">Traditional Authority</label>
+                                                        <select class="form-select" id="ta" required>
+                                                            <option selected disabled value="">Choose...</option>
+                                                            <option>...</option>
+                                                        </select>
+                                                        <div class="invalid-feedback">
+                                                            Please select a valid TA.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="mb-2">
+                                                        <label for="gvh" class="form-label">GVH</label>
+                                                        <select class="form-select" id="gvh" required>
+                                                            <option selected disabled value="">Choose...</option>
+                                                            <option>...</option>
+                                                        </select>
+                                                        <div class="invalid-feedback">
+                                                            Please select a valid Group Village Head.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="mb-3">
+                                                        <label for="village" class="form-label">Village</label>
+                                                        <select class="form-select" id="village" required>
+                                                            <option selected disabled value="">Choose...</option>
+                                                            <option>...</option>
+                                                        </select>
+                                                        <div class="invalid-feedback">
+                                                            Please select a valid Village in Malawi.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                            
+                                            <div class="form-check mb-3">
+                                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                                                <label class="form-check-label" for="invalidCheck">
+                                                    Agree to terms and conditions
+                                                </label>
+                                                <div class="invalid-feedback">
+                                                    You must agree before submitting.
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <button class="btn btn-primary" type="submit">Submit form</button>
+                                            </div>
+                                        </form>
+
+                                        <!-- start accordion -->
+                                        <div class="row">
+                                            <div class="col-xl-12">
+                                                <div class="mt-4">
+                                                    <div class="accordion" id="accordionExample">
+                                                        <div class="accordion-item">
+                                                            <h2 class="accordion-header" id="headingOne">
+                                                                <button class="accordion-button fw-medium" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                                    Add New Household
+                                                                </button>
+                                                            </h2>
+                                                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                                <div class="accordion-body">
+                                                                    <div class="text-muted">
+                                                                        <strong class="text-dark">This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    <!-- end accordion -->
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <!-- end row -->
+                                        <!-- end accordion -->
+
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                    <h6><p style="color: white; background: green; height:7mm">Basic Livelihoods: Beneficiary Households</p></h6>
+                                                        <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+                                                        
+                                                            <thead>
+                                                                <tr>
+                                                                    
+                                                                    <th>hhcode</th>
+                                                                    <th>GVH</th>
+                                                                    <th>Village</th>
+                                                                    <th>HH Name</th>
+                                                                    <th>SPP</th>
+                                                                    
+                                                                    <th>Cluster</th>
+                                                                    <th>Group</th>
+                                                                    <th>Action</th>
+                                                                   
+                                                                </tr>
+                                                            </thead>
+
+
+                                                            <tbody>
+                                                                <tr>
+                                                                    
+                                                                    <td>mzml000001</td>
+                                                                    <td>Edinburgh</td>
+                                                                    <td>61</td>
+                                                                    <td>2011/04/25</td>
+                                                                    <td>$320,800</td>
+                                                                    <td>Edinburgh</td>
+                                                                    
+                                                                    <td>$320,800</td>
+                                                                    <th>view/edit</th>
+                                                                </tr>
+                                                                <tr>
+                                                                    
+                                                                    <td>mzml000002</td>
+                                                                    <td>Tokyo</td>
+                                                                    <td>63</td>
+                                                                    <td>2011/07/25</td>
+                                                                    <td>$170,750</td>
+                                                                    <td>Edinburgh</td>
+                                                                    
+                                                                    <td>$320,800</td>
+                                                                    <th>view/edit</th>
+                                                                </tr>
+                                                                <tr>
+                                                                    
+                                                                    <td>mzml000003</td>
+                                                                    <td>San Francisco</td>
+                                                                    <td>66</td>
+                                                                    <td>2009/01/12</td>
+                                                                    <td>$86,000</td>
+                                                                    <td>Edinburgh</td>
+                                                                    
+                                                                    <td>$320,800</td>
+                                                                    <th>view/edit</th>
+                                                                </tr>
+                                                                <tr>
+                                                                    
+                                                                    <td>mzml000004</td>
+                                                                    <td>Edinburgh</td>
+                                                                    <td>22</td>
+                                                                    <td>2012/03/29</td>
+                                                                    <td>$433,060</td>
+                                                                    <td>Edinburgh</td>
+                                                                    
+                                                                    <td>$320,800</td>
+                                                                    <th>view/edit</th>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                        </p>
+                                                    </div>
+                                                </div>     
+                                            </div>            
+                                        </div>
                                     </div>
+
                                     <div class="tab-pane" id="messages-1" role="tabpanel">
                                         <p class="mb-0">
                                             Etsy mixtape wayfarers, ethical wes anderson tofu before they
@@ -191,6 +379,27 @@
 <?php include 'layouts/vendor-scripts.php'; ?>
 
 <!-- App js -->
+<script src="assets/js/app.js"></script>
+<!-- Required datatable js -->
+<script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="assets/libs/jszip/jszip.min.js"></script>
+<script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+
+<!-- Responsive examples -->
+<script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+<!-- Datatable init js -->
+<script src="assets/js/pages/datatables.init.js"></script>
+
 <script src="assets/js/app.js"></script>
 
 </body>
