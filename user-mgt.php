83958a5db166f64557c2e57ea9d501565f2d6027
witney2326 <wkabango@gmail.com>
2021-11-25 03:24:03 +0200

    additions

--- user-mgt.php
+++ user-mgt.php
@@ -1,3 +1,108 @@
+<?php
+// Include config file
+require_once "layouts/config.php";
+
+// Define variables and initialize with empty values
+$useremail = $username =  $password = $confirm_password = "";
+$useremail_err = $username_err = $password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Validate useremail
+    if (empty(trim($_POST["useremail"]))) {
+        $useremail_err = "Please enter a useremail.";
+    } elseif (!filter_var($_POST["useremail"], FILTER_VALIDATE_EMAIL)) {
+        $useremail_err = "Invalid email format";
+    } else {
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE useremail = ?";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_useremail);
+
+            // Set parameters
+            $param_useremail = trim($_POST["useremail"]);
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $useremail_err = "This useremail is already taken.";
+                } else {
+                    $useremail = trim($_POST["useremail"]);
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Validate username
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } else {
+        $username = trim($_POST["username"]);
+    }
+
+    // Validate password
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have atleast 6 characters.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate confirm password
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please enter a confirm password.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($password_err) && ($password != $confirm_password)) {
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    // Check input errors before inserting in database
+    if (empty($useremail_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (useremail, username, password, token) VALUES (?, ?, ?, ?)";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssss", $param_useremail, $param_username, $param_password, $param_token);
+
+            // Set parameters
+            $param_useremail = $useremail;
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_token = bin2hex(random_bytes(50)); // generate unique token
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Redirect to login page
+                header("location: auth-login.php");
+            } else {
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
+?>
 <?php include 'layouts/session.php'; ?>
 <?php include 'layouts/head-main.php'; ?>
 
@@ -5,7 +110,7 @@
     <title>CIMIS| User Management</title>
     <?php include 'layouts/head.php'; ?>
     <?php include 'layouts/head-style.php'; ?>
-    <?php include 'layouts/config.php'; ?>
+    
 <!-- DataTables -->
     <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
@@ -141,25 +246,40 @@
                                                                 <div class="card-body pt-0">
                                                                     
                                                                     <div class="p-2">
-                                                                        <form class="needs-validation" action="index.php">
+                                                                        <form class="needs-validation" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"">
 
-                                                                            <div class="mb-3">
-                                                                                <label for="useremail" class="form-label">User Email</label>
-                                                                                <input type="email" class="form-control" id="useremail" name="useremail" placeholder="Enter email">
+                                                                            <div class="mb-3 <?php echo (!empty($useremail_err)) ? 'has-error' : ''; ?>">
+                                                                                <label for="useremail" class="form-label">Email</label>
+                                                                                <input type="email" class="form-control" id="useremail" name="useremail" placeholder="Enter email" value="<?php echo $useremail; ?>">
+                                                                                <span class="text-danger"><?php echo $useremail_err; ?></span>
                                                                             </div>
 
-                                                                            <div class="mb-3">
+                                                                            <div class="mb-3 <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                                                                                 <label for="username" class="form-label">Username</label>
-                                                                                <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+                                                                                <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" value="<?php echo $username; ?>">
+                                                                                <span class="text-danger"><?php echo $username_err; ?></span>
                                                                             </div>
 
-                                                                            <div class="mb-3">
-                                                                                <label for="userpassword" class="form-label">Initial Password</label>
-                                                                                <input type="password" class="form-control" id="userpassword" name="password" placeholder="Enter password">
+                                                                            <div class="mb-3 <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                                                                                <label for="userpassword" class="form-label">Password</label>
+                                                                                <input type="password" class="form-control" id="userpassword" name="password" placeholder="Enter password" value="<?php echo $password; ?>">
+                                                                                <span class="text-danger"><?php echo $password_err; ?></span>
+                                                                            </div>
+
+                                                                            <div class="mb-3 <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                                                                                <label for="confirm_password" class="form-label">Confirm Password</label>
+                                                                                <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Enter confirm password" value="<?php echo $confirm_password; ?>">
+                                                                                <span class="text-danger"><?php echo $confirm_password_err; ?></span>
                                                                             </div>
 
                                                                             <div class="mt-4 d-grid">
                                                                                 <button class="btn btn-primary waves-effect waves-light" type="submit">Register</button>
+                                                                            </div>
+
+                                                                            
+
+                                                                            <div class="mt-4 text-center">
+                                                                                <p class="mb-0">By registering you agree to CIMIS <a href="#" class="text-primary">Terms of Use</a></p>
                                                                             </div>
 
                                                                             
