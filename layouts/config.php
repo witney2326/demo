634c4082c667927e08a0b6ea783d20b4000bd8d1
Jones Manganda <jonesmang300@gmail.com>
2023-03-12 00:54:44 +0200

    third commits

--- layouts/config.php
+++ layouts/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'mysql5037.site4now.net');
-define('DB_USERNAME', 'a793dc_cimis');
-define('DB_PASSWORD', 'mysql-123');
-define('DB_NAME', 'db_a793dc_cimis');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'cimis');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
