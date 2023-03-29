77c39413596802956e6d481059aa93a9fcf33bd3
Dennis794 <den.kachale@gmail.com>
2023-03-29 11:57:52 +0200

    initial commit for links

--- graduation/graduation_selected_groups_filter3.php
+++ graduation/graduation_selected_groups_filter3.php
@@ -50,7 +50,7 @@ if (($_SESSION["user_role"]== '05'))
 <!-- Begin page -->
 <div id="layout-wrapper">
 
-    <?php include '../layouts/vertical-menu.php'; ?>
+    <?php include 'layouts/vertical-menu.php'; ?>
 
     <!-- ============================================================== -->
     <!-- Start right Content here -->
